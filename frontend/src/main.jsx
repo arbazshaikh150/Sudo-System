a80a795ffe8b585227df06ec9c5cc75bfdd52bc1
arbazshaikh150 <arbazshaikh1507@gmail.com>
2026-08-07 23:44:04 +0530

    feat(frontend): wire Redux and toast providers

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
+import App from './App'
+import { store } from './store'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}><App /><ToastContainer position="bottom-right" theme="dark" /></Provider>
+  </StrictMode>,
+)
